3f11cf98812912fc08d0a84511d2a9e11ee88363
Zhe Wang <zhe.wang@instacart.com>
2020-11-25 02:46:32 -0800

    add trie router

--- main.go
+++ main.go
@@ -14,6 +14,14 @@ func main() {
 	r.GET("/hello", func(c *framework.Context) {
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
+	r.GET("/hello/:name", func(c *framework.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+	r.GET("/assets/*filepath", func(c *framework.Context) {
+		c.JSON(http.StatusOK, framework.H{
+			"filepath": c.Param("filepath"),
+		})
+	})
 	r.POST("/login", func(c *framework.Context) {
 		c.JSON(http.StatusOK, framework.H{
 			"username": c.PostForm("username"),
